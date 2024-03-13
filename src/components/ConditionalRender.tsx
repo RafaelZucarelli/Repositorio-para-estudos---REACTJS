94c94d75cd9a151dd9f1dcf6a0eb9eb19c53bbb7
RafaelZucarelli <zucarelli83@gmail.com>
2024-03-13 20:59:48 -0300

    if/else JSX

--- src/components/ConditionalRender.tsx
+++ src/components/ConditionalRender.tsx
@@ -3,11 +3,23 @@ import React, { useState } from "react";
 const ConditionalRender: React.FC = () => {
   const [x] = useState(true);
 
+  const [name] = useState("João");
+
   return (
     <div>
       <h1>Isso será exibido?</h1>
       {/*CONDIÇÃO EM REACTJS*/}
       {x && <p>Se x for true, sim!</p>}
+      <h1>Se o nome for João, o nome será exibido senão não será!</h1>
+      {name === "João" ? (
+        <div>
+          <p>O nome é: {name}</p>
+        </div>
+      ) : (
+        <div>
+          <p>O nome não será exibido pois não é "João"</p>
+        </div>
+      )}
     </div>
   );
 };
