b064aff6a9272e475f7be0d94dae5867f05e14c2
RafaelZucarelli <zucarelli83@gmail.com>
2024-03-13 20:43:06 -0300

    renderização-condicional

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ConditionalRender from "./components/ConditionalRender";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <div>
+        <ConditionalRender />
+      </div>
+    </>
+  );
+}
+
+export default App;
