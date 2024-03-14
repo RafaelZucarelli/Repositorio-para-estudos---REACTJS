feb9dfd1438c4a0acac55a816e97cab06a9ca5e6
RafaelZucarelli <zucarelli83@gmail.com>
2024-03-14 14:11:16 -0300

    renderizacaoDeListas + props

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import ShowUserName from "./components/ShowUserName";
 import CarDetails from "./components/CarDetails";
 
 function App() {
+  const cars = [
+    { id: 1, brand: "Fiat", color: "Red", newCar: true, km: 0 },
+    { id: 2, brand: "Renault", color: "Blue", newCar: true, km: 1400000 },
+    { id: 3, brand: "Nissan", color: "Black", newCar: true, km: 18000000 },
+  ];
   return (
     <>
       <h1>Hello World!</h1>
@@ -16,6 +21,15 @@ function App() {
         {/*REAPROVEITAMENTO DE COMPONENTES */}
         <CarDetails brand="VW" km={0} color="RED" newCar={true} />
         <CarDetails brand="Ford" km={1000000} color="Wiite" newCar={false} />
+        {/*LOOP EM ARRAY DE OBJETOS */}
+        {cars.map((car) => (
+          <CarDetails
+            brand={car.brand}
+            color={car.color}
+            newCar={car.newCar}
+            km={car.km}
+          />
+        ))}
       </div>
     </>
   );
